d957b2bc0728b0132682f6d87891b71e504603b1
Caio Marins <114242140+CaioMarinss@users.noreply.github.com>
2023-08-10 22:12:00 -0300

    nomes no navegador

--- paglogin/bemvindo.php
+++ paglogin/bemvindo.php
@@ -11,7 +11,7 @@ if(!$conectado){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sucesso</title>
+    <title>Bem-Vindo</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
